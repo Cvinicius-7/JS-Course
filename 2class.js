1b3544fd45b6eafa578c79bd32641f8d67adbde8
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-23 13:19:40 +0000

    add challenge code

--- 2class.js
+++ 2class.js
@@ -3,5 +3,14 @@ idade = prompt("Quantos anos você tem?");
 linguagem = prompt("Qual linguagem de programação você está estudando?");
 
 mensagem = "olá, " + nome + " você tem " + idade +  " anos e aprendendo "  + linguagem;
+pergunta = "você gosta de estudar " +linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.";
 
 console.log(mensagem);
+
+gosto = prompt(pergunta);
+
+if(gosto == 1){
+    console.log("Muito bom! Continue estudando e você terá muito sucesso.");
+}else{
+    console.log("Ahh que pena... Já tentou aprender outras linguagens?")
+}
