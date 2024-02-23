d83c7e0ec5967d94f5b0459aa329c0b198088c4e
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-23 13:12:31 +0000

    Create second class first script

---
+++ 2class.js
@@ -0,0 +1,7 @@
+nome =prompt("Qual o seu nome?");
+idade = prompt("Quantos anos você tem?");
+linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+mensagem = "olá, " + nome + " você tem " + idade +  " anos e aprendendo "  + linguagem;
+
+console.log(mensagem);
