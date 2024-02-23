d0e51fd4430386f4ca5e873a09603e43276fd68d
Carlos Vinícius Pereira <157232610+Cvinicius-7@users.noreply.github.com>
2024-02-23 13:21:13 +0000

    change resolving method

--- 2class.js
+++ 2class.js
@@ -5,12 +5,12 @@ linguagem = prompt("Qual linguagem de programação você está estudando?");
 mensagem = "olá, " + nome + " você tem " + idade +  " anos e aprendendo "  + linguagem;
 pergunta = "você gosta de estudar " +linguagem + "? Responda com o número 1 para SIM ou 2 para NÃO.";
 
-console.log(mensagem);
+alert(mensagem);
 
 gosto = prompt(pergunta);
 
 if(gosto == 1){
-    console.log("Muito bom! Continue estudando e você terá muito sucesso.");
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
 }else{
-    console.log("Ahh que pena... Já tentou aprender outras linguagens?")
+    alert("Ahh que pena... Já tentou aprender outras linguagens?")
 }
